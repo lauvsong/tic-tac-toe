9c904123ed293abbdfb81032da116c8430429794
hyesong kim <wisepine@kakao.com>
2021-09-14 17:18:08 +0900

    done

--- src/Board.js
+++ src/Board.js
@@ -3,29 +3,25 @@ import Square from './Square.js'
 
 class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return (
+        <Square 
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
+        />
+      )
     }
   
     render() {
-      const status = 'Next player: X';
-  
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            {[0,1,2].map(e=>this.renderSquare(e))}
           </div>
           <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+            {[3,4,5].map(e=>this.renderSquare(e))}
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {[6,7,8].map(e=>this.renderSquare(e))}
           </div>
         </div>
       );
--- src/Square.js
+++ src/Square.js
@@ -1,13 +1,10 @@
 import React from 'react'
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
         </button>
-      );
-    }
+    )
 }
-
 export default Square
