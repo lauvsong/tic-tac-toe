9d2253717f7dd7597f37199410030fd827622486
hyesong kim <wisepine@kakao.com>
2021-09-14 15:34:19 +0900

    Init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import './tictactoe.css'
+import Game from './Game.js'
 
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <Game></Game>
+      </main>
     </div>
   );
 }
